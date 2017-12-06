a0d6c608ce1b0722ddc7839fe18507d161e08265
Genesis Font <purewebdev@gmail.com>
2017-12-06 11:48:48 -0500

    migration file for markets table

---
+++ database/migrations/2017_12_06_155316_create_markets_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMarketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('markets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('MarketCurrency');
+            $table->string('BaseCurrency');
+            $table->string('MarketCurrencyLong');
+            $table->string('BaseCurrencyLong');
+            $table->string('MinTradeSize');
+            $table->string('MarketName');
+            $table->boolean('IsActive');
+            $table->string('Created');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('markets');
+    }
+}
